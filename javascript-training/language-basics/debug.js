965ca02d92cfd25b2d429c24819cbad8f01fe617
Bhushan Mhaske <palmh369@gmail.com>
2026-03-05 09:13:02 +0530

    Assignments

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,32 @@
+//Debugging in JavaScript 
+
+//Debugging is the process of finding and fixing errors in your code. 
+//During the debugging process, we are going to manually control and run the program line by line to identify the errors. 
+
+console.log("Executing line 1 ");
+console.log("Executing line 2 ");
+console.log("Executing line 3 ");
+sumOfNumbers(2, 4);
+console.log("Executing line 4 ");
+console.log("Executing line 5 ");
+console.log("Executing line 6 ");
+console.log("Executing line 7 ");
+console.log("Executing line 8 ");
+console.log("Executing line 9 ");
+console.log("Executing line 10 ");
+
+
+
+
+function sumOfNumbers(a, b) {
+    c = a + b;
+    console.log("sum is :" + c);
+}
+
+//VS Code Shortcuts
+
+//1. Format Document : Shift + Alt + F
+//2. Open Terminal : Cntrl+`  (backtick)
+//3. Go to specific line : Cntrl+G
+//4. Open and close the files.  Open File (Cntrl+P) , Close the File (Cntrl+W)
+//5. Rename JS Files : F2
